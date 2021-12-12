6cce1909de8dff5bf5d754d84ec6a96cd8904316
Reznixon <85173851+Reznixon@users.noreply.github.com>
2021-12-12 18:25:13 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-for (;;) console.log("hi");
+// 12.12.2021 
+
+for (;;) {
+	// mini calculator
+
+	let a = Number (prompt(" Введите первое число: "));
+	let b = Number (prompt(" Введите второе число: "));
+	let c = prompt(" Введите выражение для операции: - + * / ");
+
+	// container 
+
+	let res = (0)
+
+	//logick calculator
+
+	if (c == "+") {
+		res = (a + b);
+		alert (res);
+	} else if (c == "-") {
+		res = (a - b);
+		alert (res);
+	} else if (c == "*") {
+		res = (a * b);
+		alert (res);
+	} else if (c == "/") {
+		res = (a / b);
+		alert (res);
+	}
+
+}
